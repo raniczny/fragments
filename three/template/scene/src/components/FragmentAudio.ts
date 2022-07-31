7ef1eb657dbfa7779efbdd536defa18e819e5ab5
Ivan <ivan@raniczny.net>
2022-07-31 18:38:23 +0200

    Add default loop and volume values in FragmentAudio

--- three/template/scene/src/components/FragmentAudio.ts
+++ three/template/scene/src/components/FragmentAudio.ts
@@ -2,17 +2,17 @@ import {Audio, AudioListener} from "three"
 import {FragmentAudioLoader} from "./FragmentAudioLoader"
 
 export class FragmentAudio extends Audio {
-    constructor(audioListener: AudioListener, audioFile: string) {
+    constructor(audioListener: AudioListener, audioFile: string, looped?: boolean, initVolume?: number) {
         super(audioListener)
 
-        this.loadAudio(audioFile)
+        this.loadAudio(audioFile, looped, initVolume)
     }
 
-    private loadAudio(audioFile: string) {
+    private loadAudio(audioFile: string, looped?: boolean, initVolume?: number) {
         FragmentAudioLoader.getInstance().load(audioFile, (buffer) => {
             this.setBuffer(buffer)
-            this.setLoop(true)
-            this.setVolume(3)
+            looped ? this.setLoop(looped) : this.setLoop(false)
+            initVolume ? this.setVolume(initVolume) : this.setVolume(1)
         })
     }
 }
